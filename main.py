d5a5394a2b57f546c1f3fbfcedc613c1fb7009bf
Ilya Seleznev <zedplay.zp@gmail.com>
2025-05-12 20:05:14 +0300

    Fixed logistical mistake

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class PayoutReport:
     def __init__(self):
         self.departments = []
 
-    def add_department(self, employee: Employee):
+    def add_employee(self, employee: Employee):
         if employee.department not in self.departments:
             self.departments[employee.department] = Department(employee.department)
         self.departments[employee.department].add_employee(employee)
