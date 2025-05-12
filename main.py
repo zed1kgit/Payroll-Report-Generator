d119a73e2a9811425bcbfef81c1f4b33dd6331a2
Ilya Seleznev <zedplay.zp@gmail.com>
2025-05-12 23:02:57 +0300

    Added save functional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 
 class Employee:
@@ -34,7 +35,8 @@ class Department:
         for employee in self.employees:
             lines.append(
                 f"----------------  {employee.name:<18} {employee.hours_worked:<6}  {employee.hourly_rate:<5}  ${employee.payout:<6}")
-        lines.append(f"                                     {self.total_hours_worked():<6}         ${self.total_payout():<6}")
+        lines.append(
+            f"                                     {self.total_hours_worked():<6}         ${self.total_payout():<6}")
         return lines
 
 
@@ -70,6 +72,9 @@ class Report:
     def all_reports(cls):
         return {subclass.name: subclass for subclass in cls.__subclasses__()}
 
+    def save(self, filename, data, output_format):
+        output_format.save(filename, data)
+
 
 class PayoutReport(Report):
     name = 'payout'
@@ -82,11 +87,59 @@ class PayoutReport(Report):
             self.departments[employee.department] = Department(employee.department)
         self.departments[employee.department].add_employee(employee)
 
+    def get_data(self):
+        return {
+            department.name: {
+                "employees": [
+                    {
+                        "id": employee.id,
+                        "name": employee.name,
+                        "email": employee.email,
+                        "hours_worked": employee.hours_worked,
+                        "hourly_rate": employee.hourly_rate,
+                        "payout": employee.payout
+                    }
+                    for employee in department.employees
+                ],
+                "total_hours_worked": department.total_hours_worked(),
+                "total_payout": department.total_payout()
+            }
+            for department in self.departments.values()
+        }
+
     def generate(self):
         all_lines = ["                  name              hours   rate   payout"]
         for department in self.departments.values():
             all_lines.extend(department.report())
         return "\n".join(all_lines)
+
+
+class OutputFormat:
+    extension: str = ""
+
+    @classmethod
+    def all_formats(cls):
+        return {subclass.extension: subclass for subclass in cls.__subclasses__()}
+
+    @classmethod
+    def from_filename(cls, filename):
+        ext = filename.rsplit('.', 1)[-1].lower()
+        formats = cls.all_formats()
+        format_cls = formats[ext]
+        if not format_cls:
+            raise ValueError(f"Формат .{ext} не поддерживается. Поддерживаемые форматы: {', '.join(formats)}")
+        return format_cls()
+
+    def save(self, filename, data):
+        raise NotImplementedError
+
+
+class JSONFormat(OutputFormat):
+    extension = 'json'
+
+    def save(self, filename, data):
+        with open(f"{filename}", 'w', encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
 
 
 def main():
@@ -95,6 +148,7 @@ def main():
     parser = argparse.ArgumentParser(description="Скрипт подсчёта зарплаты сотрудников.")
     parser.add_argument('files', type=str, nargs='+', help="CSV файлы с данными сотрудников")
     parser.add_argument('--report', required=True, choices=list(reports.keys()), help="Тип отчета")
+    parser.add_argument('--save', type=str, help="Название файла для сохранения отчета (например, report.json)")
     args = parser.parse_args()
 
     report_cls = reports.get(args.report)
@@ -107,6 +161,14 @@ def main():
             report.add_employee(employee)
 
     print(report.generate())
+
+    if args.save:
+        try:
+            output_format = OutputFormat.from_filename(args.save)
+            report.save(args.save, report.get_data(), output_format)
+            print(f"Отчет успешно сохранен в файл: {args.save}")
+        except ValueError as error:
+            print(f"Ошибка: {error}")
 
 
 if __name__ == '__main__':
